237c57334445f8c72ceaacbd7f96dfb87c68f640
Léo Pradel <pradel.leo@gmail.com>
2024-04-29 17:09:57 +0200

    Update upload-metadata.post.ts

--- apps/server/src/routes/api/profile/upload-metadata.post.ts
+++ apps/server/src/routes/api/profile/upload-metadata.post.ts
@@ -64,7 +64,7 @@ export default defineEventHandler(async (event) => {
     event: "metadata uploaded",
     properties: {
       profileId: event.context.user.profileId,
-      txId: id,
+      arweaveTxId: id,
       type: "profile",
     },
   });
