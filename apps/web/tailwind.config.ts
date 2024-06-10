f6c4ea668fef5a378302b49c3d2e9c2700cfe513
Léo Pradel <pradel.leo@gmail.com>
2024-06-10 22:18:06 +0200

    feat: tiptap editor (#18)

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -2,6 +2,7 @@ import { radixThemePreset } from "radix-themes-tw";
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: "class",
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -14,6 +15,20 @@ const config: Config = {
       },
       borderRadius: {
         none: "0px",
+      },
+      typography: {
+        DEFAULT: {
+          css: {
+            "--tw-prose-hr": "var(--gray-5)",
+            "--tw-prose-invert-hr": "var(--gray-5)",
+            maxWidth: false,
+            // Center images in prose
+            img: {
+              marginLeft: "auto",
+              marginRight: "auto",
+            },
+          },
+        },
       },
     },
   },
