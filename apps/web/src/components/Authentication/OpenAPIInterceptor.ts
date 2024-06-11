c1b4e913812f208d17bb920e7d31960043d3af6b
Léo Pradel <pradel.leo@gmail.com>
2024-06-11 16:12:53 +0200

    feat: use identity token (#22)

--- apps/web/src/components/Authentication/OpenAPIInterceptor.ts
+++ apps/web/src/components/Authentication/OpenAPIInterceptor.ts
@@ -1,11 +1,11 @@
 "use client";
 
 import { OpenAPI } from "@/__generated__/opanapi/requests";
-import { useAccessToken } from "@lens-protocol/react-web";
+import { useIdentityToken } from "@lens-protocol/react-web";
 import { useEffect } from "react";
 
 export const OpenAPIInterceptor = () => {
-  const accessToken = useAccessToken();
+  const identityToken = useIdentityToken();
 
   useEffect(() => {
     const doSomethingWithRequest = (request: RequestInit) => {
@@ -16,7 +16,7 @@ export const OpenAPIInterceptor = () => {
       request.headers = {
         ...request.headers,
         "Content-Type": "application/json",
-        Authorization: `Bearer ${accessToken}`,
+        Authorization: `Bearer ${identityToken}`,
       };
       // For file uploads to work we need to remove the Content-Type header
       if (isMultiPartRequest) {
@@ -29,7 +29,7 @@ export const OpenAPIInterceptor = () => {
 
     OpenAPI.interceptors.request.use(doSomethingWithRequest);
     return () => OpenAPI.interceptors.request.eject(doSomethingWithRequest);
-  }, [accessToken]);
+  }, [identityToken]);
 
   return null;
 };
