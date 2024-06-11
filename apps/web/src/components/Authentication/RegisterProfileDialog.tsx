91d63786459bdbe84cf054dd416b1642be1d1b49
Léo Pradel <pradel.leo@gmail.com>
2024-06-11 17:36:23 +0200

    improve dialog register

--- apps/web/src/components/Authentication/RegisterProfileDialog.tsx
+++ apps/web/src/components/Authentication/RegisterProfileDialog.tsx
@@ -1,47 +1,16 @@
-import { useLensClient } from "@/hooks/use-lens-client";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { Button, Dialog, Text, TextField } from "@radix-ui/themes";
-import { IconArrowRight, IconSearch } from "@tabler/icons-react";
-import { useForm } from "react-hook-form";
-import { useAccount } from "wagmi";
-import { z } from "zod";
+import { LogoImage } from "@/images/Logo";
+import { Button, Dialog, Heading, Text } from "@radix-ui/themes";
+import { IconArrowRight } from "@tabler/icons-react";
+import Link from "next/link";
 import { useAuthenticationStore } from "./store";
 
-const registerProfileSchema = z.object({
-  username: z.string().min(5).max(26),
-});
-
-type RegisterProfileFormData = z.infer<typeof registerProfileSchema>;
-
 export const RegisterProfileDialog = () => {
-  const { address } = useAccount();
   const registerProfileOpen = useAuthenticationStore(
     (state) => state.registerProfileOpen,
   );
   const setRegisterProfileOpen = useAuthenticationStore(
     (state) => state.setRegisterProfileOpen,
   );
-  const lensClient = useLensClient();
-
-  const {
-    register,
-    handleSubmit,
-    formState: { isLoading, errors },
-  } = useForm<RegisterProfileFormData>({
-    resolver: zodResolver(registerProfileSchema),
-  });
-
-  const onSubmit = handleSubmit(async (formValues) => {
-    if (!address) return;
-
-    const result = await lensClient.wallet.createProfileWithHandle({
-      handle: formValues.username,
-      to: address,
-    });
-    console.log(result);
-
-    // TODO proper error handling and loading https://docs.lens.xyz/docs/onboarding#testnet-profiles
-  });
 
   return (
     <Dialog.Root
@@ -49,32 +18,32 @@ export const RegisterProfileDialog = () => {
       onOpenChange={setRegisterProfileOpen}
     >
       <Dialog.Content size="3" className="max-w-[450px]">
-        <Text as="div" size="2" mb="4">
-          Testnet only feature to create a Lens profile.
-        </Text>
-        <form onSubmit={onSubmit}>
-          <TextField.Root
-            size="2"
-            placeholder="Mint your handle"
-            autoComplete="off"
-            {...register("username")}
-          >
-            <TextField.Slot>
-              <IconSearch size={16} />
-            </TextField.Slot>
-          </TextField.Root>
-          {errors.username && (
-            <Text as="div" size="1" color="red" mt="1">
-              {errors.username.message}
-            </Text>
-          )}
+        <LogoImage height={28} />
 
-          <div className="mt-4 flex justify-end gap-3">
-            <Button type="submit" loading={isLoading}>
-              Mint <IconArrowRight size={16} />
-            </Button>
-          </div>
-        </form>
+        <Heading as="h2" size="4" className="mt-5">
+          Create a Lens profile
+        </Heading>
+
+        <Text className="mt-4" as="p" size="2" color="gray">
+          You need a Lens profile to use all the features of Sigle. With it you
+          can create content, start a following, and more.
+        </Text>
+
+        <Text className="mt-2" as="p" size="2" color="gray">
+          Mint your profile to get started!
+        </Text>
+
+        <Button
+          className="mt-4 w-full"
+          size="3"
+          color="gray"
+          highContrast
+          asChild
+        >
+          <Link href="/mint">
+            Mint your profile <IconArrowRight size={16} />
+          </Link>
+        </Button>
       </Dialog.Content>
     </Dialog.Root>
   );
